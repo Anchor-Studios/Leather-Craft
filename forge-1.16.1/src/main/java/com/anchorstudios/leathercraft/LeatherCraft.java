efd478ec42bedba14f9333f78c78a23acfbb1203
yigit-guven <yigitguven0124@gmail.com>
2025-05-29 14:39:29 +0300

    Created forge 1.16.x support

---
+++ forge-1.16.1/src/main/java/com/anchorstudios/leathercraft/LeatherCraft.java
@@ -0,0 +1,8 @@
+package com.anchorstudios.leathercraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(LeatherCraft.MODID)
+public class LeatherCraft {
+    public static final String MODID = "leathercraft";
+}
