fdfcff6b8ea88e1402d5ea0ec27d76bfe6e7fa43
yigit-guven <yigitguven0124@gmail.com>
2025-06-24 22:02:01 +0300

    Initial Setup for 1.21.4 NeoForge Fix

---
+++ neoforge-1.21.4/src/main/java/com/anchorstudios/leathercraft/LeatherCraft.java
@@ -0,0 +1,9 @@
+package com.anchorstudios.leathercraft;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(LeatherCraft.MODID)
+public class LeatherCraft
+{
+    public static final String MODID = "leathercraft";
+}
