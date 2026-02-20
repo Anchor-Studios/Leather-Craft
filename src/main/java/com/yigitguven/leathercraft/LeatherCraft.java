c9c1092caeee0c8f5f46ca69c3818bfaec3022c7
Yiğit Güven <contact@yigitguven.net>
2026-02-20 05:02:44 +0100

    Introduce Leather Craft mod with rotten flesh to leather recipes and initial Forge 1.20.1 build setup.

--- src/main/java/com/yigitguven/leathercraft/LeatherCraft.java
+++ src/main/java/com/yigitguven/leathercraft/LeatherCraft.java
@@ -1,13 +1,12 @@
 package com.yigitguven.leathercraft;
 
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(LeatherCraft.MODID)
 public class LeatherCraft {
     public static final String MODID = "leather_craft";
 
-    public LeatherCraft(IEventBus modEventBus) {
+    public LeatherCraft() {
         // No registration needed for vanilla recipes as they are data-driven
     }
 }
